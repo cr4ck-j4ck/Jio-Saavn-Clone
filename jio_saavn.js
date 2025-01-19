0d354649a240319d31a59a9a5c1078b6f5e26db2
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-01-19 15:16:42 +0530

    Fixed Some Bugs in JS and CSS

--- jio_saavn.js
+++ jio_saavn.js
@@ -3,7 +3,16 @@ const leftButtons = document.querySelectorAll(".fa-less-than");
 const rightButtons = document.querySelectorAll(".fa-greater-than");
 const containers = document.querySelectorAll(".container");
 
+// async function getSong(){
+//     const data = await fetch("https://aac.saavncdn.com/996/76dc0e09ab60682b243b46ab30790536_160.mp4");
+//     const song = await data.blob();
+//     const url = URL.createObjectURL(song);
+//     const audio = document.querySelector(".song");
+//     audio.src = url;
+//     console.dir(audio);
+// }
 
+// getSong();
 for (let button of leftButtons) {
     button.addEventListener("click", function () {
         if (this.nextElementSibling.scrollLeft > 0) {
@@ -29,11 +38,10 @@ containers.forEach(container => {
         if (row.scrollLeft > 0) {
             leftButton.style.opacity = 1;
         } else {
-            leftButton.style.opacity = 0.3;            
+            leftButton.style.opacity = 0.1;            
         }
-        
-        if (Math.floor(row.scrollLeft) > (row.scrollWidth - row.clientWidth) - 1) {
-            rightButton.style.opacity = 0.2;
+        if (Math.floor(row.scrollLeft) >= (row.scrollWidth - row.clientWidth) - 1) {
+            rightButton.style.opacity = 0.1;
         } else {
             rightButton.style.opacity = 1;
         }
