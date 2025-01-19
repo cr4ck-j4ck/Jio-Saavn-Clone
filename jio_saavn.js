2f95f15ca9bcc92bcbb4eaf77b4f3f4dc1dff387
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-01-19 11:52:37 +0530

    Made some changes in JS code

--- jio_saavn.js
+++ jio_saavn.js
@@ -1,19 +1,46 @@
 const row = document.querySelectorAll(".row");
 const leftButtons = document.querySelectorAll(".fa-less-than");
 const rightButtons = document.querySelectorAll(".fa-greater-than");
+const containers = document.querySelectorAll(".container");
+
 
 for (let button of leftButtons) {
     button.addEventListener("click", function () {
-        if(this.nextElementSibling.scrollLeft > 0 ){
-            this.nextElementSibling.scrollTo({ left: 0, behavior: 'smooth' }) 
+        if (this.nextElementSibling.scrollLeft > 0) {
+            this.nextElementSibling.scrollTo({ left: 0, behavior: 'smooth' })
         }
     });
 }
 
 for (let button of rightButtons) {
     button.addEventListener("click", function () {
-        if(Math.floor(this.previousElementSibling.scrollLeft) !==  ((this.previousElementSibling.scrollWidth - row.clientWidth)- 1)){
-            this.previousElementSibling.scrollTo({ left: this.previousElementSibling.scrollWidth, behavior: 'smooth' }) 
+        const row = this.previousElementSibling; {
+            row.scrollTo({ left: row.scrollWidth, behavior: 'smooth' })
         }
-    });    
+    });
 }
+
+containers.forEach(container => {
+    const row = container.querySelector(".row");
+    const leftButton = container.querySelector(".fa-less-than");
+    const rightButton = container.querySelector(".fa-greater-than");
+
+    container.addEventListener("mouseenter", function () {
+        if (row.scrollLeft > 0) {
+            leftButton.style.opacity = 1;
+        } else {
+            leftButton.style.opacity = 0.3;            
+        }
+        
+        if (Math.floor(row.scrollLeft) > (row.scrollWidth - row.clientWidth) - 1) {
+            rightButton.style.opacity = 0.2;
+        } else {
+            rightButton.style.opacity = 1;
+        }
+    });
+
+    container.addEventListener("mouseleave", function () {
+        leftButton.style.opacity = 0;
+        rightButton.style.opacity = 0;
+    });
+})
