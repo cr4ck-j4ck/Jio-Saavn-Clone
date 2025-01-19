f94a692e9ce4de98e267e0d4420b5d7c31a40072
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-01-19 11:10:54 +0530

    Added Playbutton and responsive Styling of playbutton

--- jio_saavn.js
+++ jio_saavn.js
@@ -2,10 +2,8 @@ const row = document.querySelectorAll(".row");
 const leftButtons = document.querySelectorAll(".fa-less-than");
 const rightButtons = document.querySelectorAll(".fa-greater-than");
 
-
 for (let button of leftButtons) {
     button.addEventListener("click", function () {
-
         if(this.nextElementSibling.scrollLeft > 0 ){
             this.nextElementSibling.scrollTo({ left: 0, behavior: 'smooth' }) 
         }
