4c1ce83f5cb633528662c000e2487a6459c6fa21
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-01-20 09:11:19 +0530

    Added JSON file which contains the data of the song like Image URL and Song URL and Wrote some code of playing the song.

--- jio_saavn.js
+++ jio_saavn.js
@@ -1,18 +1,44 @@
 const row = document.querySelectorAll(".row");
 const leftButtons = document.querySelectorAll(".fa-less-than");
 const rightButtons = document.querySelectorAll(".fa-greater-than");
-const containers = document.querySelectorAll(".container");
+const containers = document.querySelectorAll(".scrollable");
+const playButtons = document.querySelectorAll(".play-button");
+const currentSongImage = document.querySelector("#currentImage");
+const songDetails = document.querySelector(".song-details");
 
-// async function getSong(){
-//     const data = await fetch("https://aac.saavncdn.com/996/76dc0e09ab60682b243b46ab30790536_160.mp4");
-//     const song = await data.blob();
-//     const url = URL.createObjectURL(song);
-//     const audio = document.querySelector(".song");
-//     audio.src = url;
-//     console.dir(audio);
-// }
+async function getSongData() {
+    const data = await fetch('./songsData.json')
+    return await data.json();
+}
 
-// getSong();
+document.querySelector(".content").addEventListener("keydown",function(){
+    console.log("clicked");
+});
+
+async function playSong(src) {
+    const songData = await getSongData();
+    const songObj = songData.find(el =>{
+        return el.songImage === src;
+    });
+
+    const data = await fetch(songObj.songURL);
+    const song = await data.blob();
+    const url = URL.createObjectURL(song);
+    const audio = document.querySelector("audio");
+    audio.src = url;
+    audio.play();
+    currentSongImage.src = songObj.songImage;
+    songDetails.querySelector("h3").innerText = songObj.songName    
+    songDetails.querySelector("p").innerText = songObj.songDetail
+}
+
+for (let button of playButtons) {
+    button.addEventListener("click", async function () {
+        console.log(this.parentElement.firstElementChild.src);
+        await playSong(this.parentElement.firstElementChild.src);
+    });
+}
+
 for (let button of leftButtons) {
     button.addEventListener("click", function () {
         if (this.nextElementSibling.scrollLeft > 0) {
@@ -38,7 +64,7 @@ containers.forEach(container => {
         if (row.scrollLeft > 0) {
             leftButton.style.opacity = 1;
         } else {
-            leftButton.style.opacity = 0.1;            
+            leftButton.style.opacity = 0.1;
         }
         if (Math.floor(row.scrollLeft) >= (row.scrollWidth - row.clientWidth) - 1) {
             rightButton.style.opacity = 0.1;
